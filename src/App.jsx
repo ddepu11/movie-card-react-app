32fb7e7edeabc4d60cc3088d2ba2e44685554d52
Deepanshu Tiwari <ddepu11@gmail.com>
2021-03-04 14:33:56 +0530

    image height reduced and margin top reduced

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,20 @@
 import Card from "./Cards";
 import Navbar from "./Navbar";
+import movies from "./movie-data";
 
 function App() {
   return (
     <div className="container">
       <Navbar />
       <div className="cards">
-        <Card
-          src="https://wallpapercave.com/uwp/uwp374924.jpeg"
-          name="Dark"
-          address="https://www.netflix.com/in/title/80100172"
-          title="A Netflix Orignal Series"
-        />
-        <Card
-          src="https://wallpapercave.com/uwp/uwp374924.jpeg"
-          name="Dark"
-          address="https://www.netflix.com/in/title/80100172"
-          title="A Netflix Orignal Series"
-        />
-        <Card
-          src="https://wallpapercave.com/uwp/uwp374924.jpeg"
-          name="Dark"
-          address="https://www.netflix.com/in/title/80100172"
-          title="A Netflix Orignal Series"
-        />
+        {movies.map((movie) => (
+          <Card
+            src={movie.src}
+            name={movie.name}
+            address={movie.address}
+            title={movie.title}
+          />
+        ))}
       </div>
     </div>
   );
--- src/movie-data.js
+++ src/movie-data.js
@@ -1,8 +1,35 @@
 const data = [
   {
-    src: "",
-    name: "",
-    address: "",
-    title: "",
+    src: "https://wallpapercave.com/wp/wp1839578.jpg",
+    name: "Stranger Thinngs",
+    address: "https://www.netflix.com/in/title/80057281",
+    title: "Netflix Orignal Series",
+  },
+  {
+    src: "https://wallpapercave.com/wp/wp4906374.jpg",
+    name: "Money Heist",
+    address: "https://www.netflix.com/in/title/80192098",
+    title: "Netflix Orignal Series",
+  },
+  {
+    src: "https://wallpapercave.com/wp/wp2096857.jpg",
+    name: "Black Mirror",
+    address: "https://www.netflix.com/title/70264888",
+    title: "Netflix Orignal Series",
+  },
+  {
+    src: "https://wallpapercave.com/wp/wp7285343.jpg",
+    name: "Mirzapur",
+    address:
+      "https://www.primevideo.com/detail/amzn1.dv.gti.02ba3688-c4d2-83b3-bcf4-c7a85b36aef4/ref_=dvm_pds_gen_in_as_s_gt_mirza2%7Cm_IPYGGcHWc_c474694821599?gclid=CjwKCAiAp4KCBhB6EiwAxRxbpM9bNhgEySZx7LJDHff6-VqAjrKXjoxAReo-1rGpQUygIcpoE0VBaRoCOW4QAvD_BwE",
+    title: "Amazon Prime",
+  },
+  {
+    src: "https://wallpapercave.com/wp/wp4025657.jpg",
+    name: "Sacred Games",
+    address: "https://www.netflix.com/in/title/80115328",
+    title: "Amazon Prime",
   },
 ];
+
+export default data;
