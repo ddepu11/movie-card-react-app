9ba6cf95e707cc46cacb5d2fd99c15343d17f6f8
Deepanshu Tiwari <ddepu11@gmail.com>
2021-03-04 13:02:28 +0530

    hover effect

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -2,9 +2,9 @@ function Cards({ src, name, address, title }) {
   return (
     <div className="card">
       <img src={src} alt="pic" className="card__img" />
-      <div className="card__info">
-        <span className="card__category">{title}</span>
-        <h3 className="card__title">{name}</h3>
+      <div className="movie-info">
+        <span className="heading">{title}</span>
+        <h3 className="movie-name">{name}</h3>
         <a className="btn" href={address} rel="noreferrer" target="_blank">
           Watch Now
         </a>
